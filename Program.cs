5606c4e69583f00592d83be35cb3eb785605c69d
Lune2003 <nekomoon2003@gmail.com>
2023-05-26 21:45:11 -0300

    dor de cabeça

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("---Simulador de luz de advertência---\n");
+Console.WriteLine("[A]tivado,[D]esativado");
+ 
+ Console.WriteLine ("sensor de temperatura...:");
+ string entradaT =
+ Console.ReadLine()?.Trim().ToUpper();
+
+ Console.WriteLine ("Sensor de Pressão..:");
+ string entradaP =
+ Console.ReadLine()?.Trim().ToUpper();
+
+ Console.WriteLine ("Sensor de rotação..:");
+ string entradaR =
+Console.ReadLine()?.Trim().ToUpper();
+
+
+string Luz_Advertencial
+Luz_Advertencial= entradaT == "A"&& (entradaP=="A" || entradaR=="A");
+
+Console.WriteLine(Luz_Advertencial de advertencia (ativada:desativada))
